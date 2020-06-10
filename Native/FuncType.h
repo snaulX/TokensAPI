5f17ed59daaefcaaeb2f032446fc652f1007b4f2
snaulX <alexandergunger666@gmail.com>
2020-06-10 19:05:10 +0300

    Finish native TokensAPI for creating tokens

--- Native/FuncType.h
+++ Native/FuncType.h
@@ -1,3 +1,12 @@
 typedef enum {
-	FuncDefault = 0
+	FuncDefault = 0,
+	FuncAbstract = 1,
+	FuncStatic = 2,
+	Final = 3,
+	Virtual = 4,
+	FuncAlias = 5,
+	Constructor = 6,
+	Operator = 7,
+	Destructor = 8,
+	Override = 9
 } FuncType;
